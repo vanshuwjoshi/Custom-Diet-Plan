a35a3774b9f2d0d1e2379f0ae68a53b48469fe0f
Vansh <vanshjoshi612@gmail.com>
2024-01-19 13:48:12 -0500

    openai api

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
 import streamlit as st
-import pandas as pd
+from langchain_openai import OpenAI
+from langchain.prompts import PromptTemplate
+import os
+from dotenv import load_dotenv
 
-st.title("Diet Generating Application")
+load_dotenv()
+
+llm = OpenAI(temperature=0.0, openai_api_key=os.getenv("OPENAI_API_KEY"))
+
+
+def get_openai_response(question):
+    response = llm.invoke(question)
+    return response
+
+
+question_string = "I am a {age} {gender}. My calorie goal for the day is {required_calories}. I am a {dietry}. I want to have {number_of_meals} meals in a day. Could you please suggest me a meal plan for today?"
+
+prompt_template = PromptTemplate(
+    input_variables=["age", "gender", "required_calories", "dietry", "number_of_meals"],
+    template=question_string,
+)
+
+st.title("Get Your Custom Diet Plan")
 
 col1, col2 = st.columns(2)
 
@@ -24,6 +44,13 @@ with st.form("calorie_form"):
         ["Sedentary", "Moderately active", "Highly active", "Extremely active"],
         key="activity_level",
     )
+    dietry = col1.selectbox(
+        "Your dietry restrictions",
+        ["Vegan", "Vegetarian", "Non-vegetarians"],
+        key="dietry_restrictions",
+    )
+
+    number_of_meals = col1.slider("Number of meals you want to eat in a day", 2, 7, 3)
 
 if col1.button("Calculate"):
     bmr = (10 * current_weight) + (6.25 * current_height) - (5 * age)
@@ -53,46 +80,12 @@ if col1.button("Calculate"):
     col2.write(
         "** Keep in mind that this is just an estimate and your actual calorie needs may vary depending on your activity level, metabolism, and other factors. It is always best to consult with a healthcare professional or registered dietitian to determine a personalized calorie goal for your specific needs and goals."
     )
-
-    dietry = col2.selectbox(
-        "Your dietry restrictions",
-        ["Vegan", "Vegetarian", "Non-vegetarians"],
-        key="dietry_restrictions",
+    question = prompt_template.format(
+        age=age,
+        gender=gender,
+        required_calories=required_calories,
+        dietry=dietry,
+        number_of_meals=number_of_meals,
     )
-
-    number_of_meals = col2.slider("Number of meals you want to eat in a day", 2, 7, 3)
-
-    col2.button("Generate Diet Plan")
-
-
-# with st.form("my_form"):
-#     name = col1.text_input("Your name", key="name")
-#     age = col1.number_input("Your age", 0, 100, key="age")
-#     current_weight = col1.number_input(
-#         "Your current weight(in Kg)", 10, 200, key="current_weight"
-#     )
-#     desired_weight = col1.number_input(
-#         "Your desired weight(in Kg)", 10, 200, key="desired_weight"
-#     )
-#     gender = col1.selectbox("Your gender", ["Male", "Female"], key="gender")
-#     dietry = col1.selectbox(
-#         "Your dietry restrictions",
-#         ["Vegan", "Vegetarian", "Non-vegetarians"],
-#         key="dietry_restrictions",
-#     )
-
-# if col1.button("Generate"):
-#     col2.write("Hi " + name + "!")
-#     prompt_string = (
-#         "You are a "
-#         + str(age)
-#         + " year old "
-#         + gender
-#         + ". Your current weight is "
-#         + str(current_weight)
-#         + " and your desired weight is "
-#         + str(desired_weight)
-#         + "."
-#     )
-#     col2.write(prompt_string)
-#     col2.write("We will use this information to genrate your custom diet.")
+    response = get_openai_response(question=question)
+    col2.write(response)
